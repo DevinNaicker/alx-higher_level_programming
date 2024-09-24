6e83d31490fee77f449f793a003cefa3808dbaed
Devin Naicker <devinnaicker@gmail.com>
2024-09-24 11:10:26 -0700

    8

---
+++ 0x01-python-if_else_loops_functions/00-test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "apple"
+for x in str:
+    print('{}'.format(ord(x)))
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for x in str:
+        if 97 <= ord(x) <= 122:
+            print('{}'.format(chr(ord(x) - 32)), end="")
+
+        else:
+            print('{}'.format(x), end="")
