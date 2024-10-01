2436855186d928189354292f71a450f65731e3bc
Devin Naicker <devinnaicker@gmail.com>
2024-10-01 04:12:39 -0700

    9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    digit = abs(number) % 10
+    if number < 0:
+        digit = -digit
+        print(digit)
+        return(digit)
+
+    else:
+        print(digit)
+        return(digit)
